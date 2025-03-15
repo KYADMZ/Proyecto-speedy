08b304087aae9910684743bc8f0a10dd212be0db
KYADMZ <DMz210@outlook.com>
2025-03-15 00:47:36 -0600

    si

--- speedy/app/page.tsx
+++ speedy/app/page.tsx
@@ -4,14 +4,18 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <div className="contenedor" data-info="Speedy a tu alcance">
+        
         <Image
-          className="dark:invert"
+          className="imagen"
           src="/speedyIcon.jpeg"
           alt="Next.js logo"
           width={580}
           height={128}
           priority
         />
+        </div>
+        
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
