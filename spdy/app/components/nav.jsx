d7ef9a2a03d7de21e8fa8b2b2a33aa80fcf5e1d4
JesusPalacios24 <jpalaciosmendias0@gmail.com>
2025-03-16 02:15:54 -0600

    se agrego el nav base

---
+++ spdy/app/components/nav.jsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+    <nav className="bg-blue-900 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href="/" className="text-white text-lg font-semibold relative group w-[200px] h-[228px]">
+            <img src="/speedyIcon.jpeg" alt="logo" />
+        </Link>
+        <div className="space-x-4">
+          <Link href="/about" className="text-gray-300 hover:text-white">
+            About
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
