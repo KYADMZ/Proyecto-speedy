80d2d6eb8041b06e35a0b92fb697dbffb2196859
KYADMZ <118216927+KYADMZ@users.noreply.github.com>
2025-03-19 13:33:03 -0600

    Ajustes después de build local

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    experimental: {
+      appDir: true, // Habilita el uso de App Router (si aplicable)
+    },
+    reactStrictMode: true, // Asegura buenas prácticas en React
+  };
+  
+  export default nextConfig;
+  
